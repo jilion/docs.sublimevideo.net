1c866bb4ae5faf8260f73e575a90ce37c5c17d4d
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-03-19 14:10:19 +0100

    Fix route contraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 class DocsPages
   def self.matches?(request)
-    pages = Dir.glob('app/views/pages/**/*.html.haml').map { |p| p[%r(app/views/pages/(.*)\.html\.haml)][1] }
+    pages = Dir.glob('app/views/pages/**/*.html.haml').map { |p|
+      p.match(%r(app/views/pages/(.*)\.html\.haml))[1]
+    }
     pages.include?(request.params["page"])
   end
 end
@@ -11,8 +13,7 @@ DocsSublimeVideo::Application.routes.draw do
 
   resources :releases, only: :index
 
-  # get '/*page' => 'pages#show', as: :page, constraints: DocsPages, format: false
-  get '/*page' => 'pages#show', as: :page, format: false
+  get '/:page' => 'pages#show', as: :page, constraints: DocsPages, format: false
 
   root to: redirect('/quickstart-guide')
 end
