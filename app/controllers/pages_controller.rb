ccbaccbf5f117c013b23e1b5707801e4218d3f28
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-03-20 15:39:27 +0100

    Add http caching to page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,19 @@ class PagesController < ApplicationController
   def show
     @body_class = params[:page]
 
-    # expires_in 30.seconds, public: true
-    render "pages/#{params[:page]}"
+    if stale?(etag: page_md5, last_modified: page_file.mtime, public: true)
+      render "pages/#{params[:page]}"
+    end
+  end
+
+private
+
+  def page_md5
+    Digest::SHA1.file(page_file).to_s
+  end
+
+  def page_file
+    @page_file ||= File.new(Rails.root.join("app/views/pages/#{params[:page]}.html.haml"))
   end
 
 end
