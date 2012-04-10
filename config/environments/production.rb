47c33bf8d2eff1b916eaa0609d363663a4e79149
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-04-10 15:38:52 +0200

    Insert Rack::GoogleAnalytics before Rack::Cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 DocsSublimeVideo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.middleware.use Rack::GoogleAnalytics, tracker: 'UA-10280941-8'
+  config.middleware.insert_before Rack::Cache, Rack::GoogleAnalytics, tracker: 'UA-10280941-8'
   config.middleware.insert_before Rack::Cache, Rack::SslEnforcer, except_hosts: 'docs.sublimevideo.net', strict: true
 
   # Code is not reloaded between requests
