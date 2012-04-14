1302b0bcd0ffb08d0a00ca92b1c8717274c1e9c2
Rémy Coutable <remy@jilion.com>
2012-04-14 17:36:57 +0200

    Update sublimevideo_layout

--- app/helpers/docs_helper.rb
+++ app/helpers/docs_helper.rb
@@ -7,9 +7,4 @@ module DocsHelper
     end
   end
 
-  # lame hack until sublimevideo_layout 0.6.6 is released...
-  def sublimevideo_include_tag(ssl_request, name)
-    %(<script src="#{ssl_request ? "https://4076.voxcdn.com" : "http://cdn.sublimevideo.net"}/js/#{::SiteToken[name.to_sym]}.js" type="text/javascript"></script>)
-  end
-
 end
