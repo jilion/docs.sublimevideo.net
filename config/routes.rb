65eb1a01e7024b7277762b918414c88f85be34a1
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-04-18 20:55:50 +0200

    Try to fix / redirect

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ class DocsPages
 end
 
 DocsSublimeVideo::Application.routes.draw do
+  root to: redirect('/quickstart-guide')
+
   # Deprecated routes
   %w[javascript-api js-api].each { |r| get r => redirect('/javascript-api/usage') }
   get 'put-video-in-a-floating-lightbox' => redirect('/lightbox')
+
   resources :releases, only: :index
-
   get '/*page' => 'pages#show', as: :page, constraints: DocsPages, format: false
-
-  root to: redirect('/quickstart-guide')
 end
