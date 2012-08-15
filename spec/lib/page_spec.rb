2acf314577088c0130ed01ebb4a0cea541b9b11e
Rémy Coutable <remy@jilion.com>
2012-08-15 12:56:52 +0200

    Ignore config/en.yml

--- spec/lib/page_spec.rb
+++ spec/lib/page_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Page do
 
-  describe 'title' do
+  describe '.menu_title' do
     context 'MENU_PAGE_TITLES is empty' do
       before do
         stub_const 'Page::MENU_PAGE_TITLES', {}
       end
 
       it 'fallbacks to humanize (with dashes replaced by spaces) if title is not found in the MENU_PAGE_TITLES hash' do
-        Page.title('javascript-api/methods-foo').should eq 'Methods foo'
+        Page.menu_title('javascript-api/methods-foo').should eq 'Methods foo'
       end
     end
 
@@ -19,7 +19,7 @@ describe Page do
       end
 
       it 'uses the title found in MENU_PAGE_TITLES hash' do
-        Page.title('foo-bar').should eq 'Foo is Bar'
+        Page.menu_title('foo-bar').should eq 'Foo is Bar'
       end
     end
   end
