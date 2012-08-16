4372a53d2319655b8e7144292ca2dfed92c79bd0
Rémy Coutable <remy@jilion.com>
2012-08-16 12:54:07 +0200

    Redirect at the Rack level to ensure redirect + cache doesn't fuckize!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    if request.post?
-      redirect_to search_url(q: h(params[:q]))
-    else
-      SearchKeyword.increment(h(params[:q]))
-      @results = Search.search("title:#{h(params[:q])}^2 OR text:#{h(params[:q])}", fetch: 'title')
-    end
+    SearchKeyword.increment(h(params[:q]))
+    @results = Search.search("title:#{h(params[:q])}^2 OR text:#{h(params[:q])}", fetch: 'title')
   end
 
 end
