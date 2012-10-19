0159388d699fb76c33ff15151e65994384f6cad2
Rémy Coutable <remy@jilion.com>
2012-10-19 20:38:23 +0200

    Update lightbox and logo page

--- spec/routings/releases_routing_spec.rb
+++ spec/routings/releases_routing_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ReleasesController do
 
-  it { get('/releases').should route_to('releases#index') }
+  it { get('/releases').should      route_to('releases#index', stage: 'stable') }
+  it { get('/beta/releases').should route_to('releases#index', stage: 'beta') }
 
 end
