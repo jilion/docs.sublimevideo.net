18a7ff2b9417fb6e06114afc88406f1bde6f0545
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-02 16:51:18 +0100

    Use EdgeCast for assets host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ DocsSublimeVideo::Application.configure do
   }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://d1p69vb2iuddhr.cloudfront.net"
+  config.action_controller.asset_host = "//cdn.sublimevideo.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
