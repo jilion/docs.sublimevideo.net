c381b62a69bb9c07eb5021e3916b9c5ca4767f7d
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-03 17:01:34 +0100

    Insert Rack::Auth before Rack::Cache

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,9 +1,9 @@
 DocsSublimeVideo::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
-  config.middleware.insert_after Rack::Lock, Rack::Auth::Basic, "Staging" do |u, p|
+  config.middleware.insert_after Rack::Cache, Rack::Auth::Basic, "Staging" do |u, p|
     [u, p] == ['jilion', ENV['PRIVATE_CODE']]
   end
-  config.middleware.insert_before Rack::Lock, Rack::SslEnforcer, except_hosts: 'docs.sublimevideo-staging.net', strict: true
+  config.middleware.insert_before Rack::Cache, Rack::SslEnforcer, except_hosts: 'docs.sublimevideo-staging.net', strict: true
 
   # One-line logs
   config.lograge.enabled = true
