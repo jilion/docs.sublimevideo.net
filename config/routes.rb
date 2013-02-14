d2e41bb115bbedd0556b20c2cae63d2c0bbb71fa
Rémy Coutable <remy@jilion.com>
2013-02-14 16:21:57 +0100

    Remove beta pages and update all links

--- config/routes.rb
+++ config/routes.rb
@@ -5,29 +5,41 @@ class DocsPages
 end
 
 DocsSublimeVideo::Application.routes.draw do
+  get '/beta/*path' => redirect { |params, _| "/#{params[:path]}" }
+
   # Legacy redirect
-  get '/autoplay-video-upon-page-load' => redirect('/autoplay')
-  get '/loop-a-video' => redirect('/loop')
   get '/put-video-in-a-floating-lightbox' => redirect('/lightbox')
-  get '/returning-to-the-initial-state-once-video-playback-ends' => redirect('/back-to-initial-state-on-end')
+  %w[autoplay-video-upon-page-load autoplay].each do |path|
+    get "/#{path}" => redirect { "/settings/player-settings#autoplay" }
+  end
+  %w[returning-to-the-initial-state-once-video-playback-ends back-to-initial-state-on-end loop-a-video loop].each do |path|
+    get "/#{path}" => redirect { "/settings/player-settings#on-end" }
+  end
   get '/ssl' => redirect('/player-faq#use-sublimevideo-in-ssl-site')
   get '/video-code-generator' => redirect('/video-publishing-assistant')
   get '/supported-browsers-and-platforms' => redirect('/supported-platforms')
-  get '/beta/embeds' => redirect('/beta/addons/embed')
-  get '(/:stage)/faq' => redirect { |params, req| "#{"/#{params[:stage]}" if params[:stage]}/player-faq" }, stage: /stable|beta/
-  get '(/:stage)/real-time-stats' => redirect { |params, req| "#{"/#{params[:stage]}" if params[:stage]}/addons/stats" }, stage: /stable|beta/
-  get '(/:stage)/optimize-for-stats' => redirect { |params, req| "#{"/#{params[:stage]}" if params[:stage]}/addons/stats#optimize-for-stats" }, stage: /stable|beta/
-  get '(/:stage)/addons/optimize-for-stats' => redirect { |params, req| "#{"/#{params[:stage]}" if params[:stage]}/addons/stats#optimize-for-stats" }, stage: /stable|beta/
+
+
+  get '(/:stage)/embeds' => redirect('/addons/embed')
+  get '(/:stage)/faq' => redirect { |params, _| "#{"/#{params[:stage]}" if params[:stage]}/player-faq" }, stage: /stable|beta/
+  get '(/:stage)/real-time-stats' => redirect { |params, _| "#{"/#{params[:stage]}" if params[:stage]}/addons/stats" }, stage: /stable|beta/
+  get '(/:stage)/optimize-for-stats' => redirect { |params, _| "#{"/#{params[:stage]}" if params[:stage]}/addons/stats#optimize-for-stats" }, stage: /stable|beta/
+  get '(/:stage)/addons/optimize-for-stats' => redirect { |params, _| "#{"/#{params[:stage]}" if params[:stage]}/addons/stats#optimize-for-stats" }, stage: /stable|beta/
 
   # Shortcut redirect
-  %w[javascript-api js-api].each { |path| get "(/:stage)/#{path}" => redirect { |params, req| "#{"/#{params[:stage]}" if params[:stage]}/javascript-api/usage" }, stage: /stable|beta/ }
-  %w[settings].each { |path| get "(/:stage)/#{path}" => redirect { |params, req| "#{"/#{params[:stage]}" if params[:stage]}/settings/usage" }, stage: /stable|beta/ }
-  %w[customizing-the-initial-play-button custom-play-button].each { |path| get "(/:stage)/#{path}" => redirect { |params, req| "#{"/#{params[:stage]}" if params[:stage]}/custom-start-view" }, stage: /stable|beta/ }
+  %w[javascript-api js-api].each do |path|
+    get "/#{path}" => redirect { |params, _| "/javascript-api/usage" }
+  end
+  %w[settings].each do |path|
+    get "/#{path}" => redirect { |params, _| "/settings/usage" }
+  end
+  %w[customizing-the-initial-play-button custom-play-button].each do |path|
+    get "/#{path}" => redirect { |params, _| "/custom-start-view" }
+  end
 
   get '(/:stage)/search' => 'search#index', as: 'search', stage: /stable|beta/, defaults: { stage: 'stable' }
   get '(/:stage)/releases' => 'releases#index', stage: /stable|beta/, defaults: { stage: 'stable' }, as: 'releases'
   get '(/:stage)/*page' => 'pages#show', as: 'page', constraints: DocsPages, stage: /stable|beta/, defaults: { stage: 'stable' }, format: false
 
-  get '/beta', to: redirect('/beta/quickstart-guide')
   root to: 'pages#redirect_from_root'
 end
--- spec/routing/releases_routing_spec.rb
+++ spec/routing/releases_routing_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe ReleasesController do
 
-  it { expect(get('/releases')).to      route_to('releases#index', stage: 'stable') }
-  it { expect(get('/beta/releases')).to route_to('releases#index', stage: 'beta') }
+  it { expect(get('/releases')).to route_to('releases#index', stage: 'stable') }
 
 end
