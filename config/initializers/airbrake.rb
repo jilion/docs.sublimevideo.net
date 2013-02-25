fed5a5866ef86a8b1c6d3f31d81b28d243df84d5
Rémy Coutable <remy@jilion.com>
2013-02-25 16:22:33 +0100

    Use Airbrake v2

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,5 @@
 Airbrake.configure do |config|
   config.api_key = '66d9c59629ba4f51757560b2a36d808e'
+  config.host    = 'collect.airbrake.io'
   config.ignore  << ActionController::UnknownHttpMethod
 end
