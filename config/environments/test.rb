8d162b67703abf6eb87649651332f55deb85bfcd
Rémy Coutable <remy@rymai.me>
2013-05-02 18:19:42 +0200

    Update gems and upgrade to Rails 4!

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,8 @@
 DocsSublimeVideo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.middleware.insert_before Rack::Lock, Rack::Nocache
+  config.middleware.insert_before Rack::Runtime, Rack::Nocache
+
+  config.eager_load = false
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -11,9 +13,6 @@ DocsSublimeVideo::Application.configure do
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
-
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,8 +1,9 @@
-require File.expand_path('../session_secret', __FILE__)
+require File.expand_path('session_secret', __dir__)
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DocsSublimeVideo::Application.config.secret_token = SessionSecret.token
+DocsSublimeVideo::Application.config.secret_token    = SessionSecret.token
+DocsSublimeVideo::Application.config.secret_key_base = SessionSecret.key_base
