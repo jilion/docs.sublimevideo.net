3d3ded89697a4f55a281792c8faf5ce6358b9798
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2014-01-16 11:45:27 +0100

    Try to fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,9 +21,10 @@ DocsSublimeVideo::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.version = '1.1'
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
